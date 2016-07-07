9bc9014c9db221bf941ed084c63794a95aaeea6c
Rafael Bautista <rafa@saras-MacBook-Pro.local>
2016-07-07 14:20:12 -0700

    Added initial wiki parser

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -11,41 +11,69 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
+
 	final static WikiFetcher wf = new WikiFetcher();
-	
+
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
-	 * 
+	 *
 	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
-	 * 
+	 *
 	 * 1. Clicking on the first non-parenthesized, non-italicized link
      * 2. Ignoring external links, links to the current page, or red links
      * 3. Stopping when reaching "Philosophy", a page with no links or a page
      *    that does not exist, or when a loop occurs
-	 * 
+	 *
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+		List<String> urls = new ArrayList<String>();
+		String current_url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		urls.add(current_url);
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+		while(!current_url.equals("")){
+
+			//checks if url is Wiki Philosophy page
+			if(current_url.equals("https://en.wikipedia.org/wiki/Philosophy")) {
+				System.out.println("Success");
+				break;
 			}
-        }
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+			Elements paragraphs = wf.fetchWikipedia(url);
+			Element firstPara = paragraphs.get(0);
+
+			int parenthesisCounter = 0;
+
+			Iterable<Node> iter = new WikiNodeIterable(firstPara);
+
+			for (Node node: iter) {
+				if(node instanceof TextNode){
+						if(node.toString().contains("("))
+							parenthesisCounter++;
+						else if(node.toString().contains(")"))
+							parenthesisCounter--;
+				}
+				if(!node.attr("href").equals("") && parenthesisCounter == 0){
+					if(urls.contains(node.attr("abs:href")){
+						url="";
+					}
+					else {
+						urls.add(node.attr("abs:href"));
+						url = node.attr("abs:href");
+						break;
+					}
+				}
+	    }
+			if(url.equals(""))
+				System.out.println("Failure");
+		}
+
+		for(String s: urls){
+			System.out.println(s);
+		}
+
+
 	}
 }
